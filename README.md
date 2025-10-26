f0382e61aea93d925332112ca36c14813f4a5cb6
Jejen Jaenudin <1125170031@global.ac.id>
2025-10-26 19:27:19 +0700

    membuat class MyHome

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Projeck week 5
 ## Getting Started
 1. Membuat my app
 2. membuat StatelessWidget
+3. membuat class MyHome
 "# Tugas_week_5_-1125170031-" 
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+    Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+    title : "title aplikasi latihan container",
+    home : MyHome(),
+    );
+  }
+}
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Container();
   }
